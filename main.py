97de8909984a7d250238dfb84cb36368dfbae70f
Justin <justinhuang0208@gmail.com>
2021-09-10 08:08:30 +0800

    POW

--- main.py
+++ main.py
@@ -4,14 +4,37 @@ class Block():
     def __init__(self, data, previousHash):
         self.data = data
         self.previousHash = previousHash
+        self.nonce = 1
         self.hash = self.compute_hash()
     
     def compute_hash(self):
-        return hashlib.sha256(self.data.encode('utf-8')).hexdigest()
+        material = self.data + self.previousHash + str(self.nonce)
+        return hashlib.sha256(material.encode('utf-8')).hexdigest()
+    
+    def print_nonce(self):
+        print(self.nonce)
+    
+    def get_answer(self, difficulty):
+        answer = ""
+        for i in range(difficulty):
+            answer = answer + "0"
+        return answer
+
+    def mine(self, difficulty):
+        while True:
+            self.hash = self.compute_hash()
+            if self.hash[0:difficulty] != self.get_answer(difficulty):
+                self.nonce += 1
+                self.hash = self.compute_hash()
+            else:
+                break
+        #print("done", self.hash)
+
 
 class Chain():
     def __init__(self):
         self.chain = [self.big_bang()]
+        self.difficulty = 5
 
     def big_bang(self):
         fatherBlock = Block("I am father", "")
@@ -22,7 +45,7 @@ class Chain():
     
     def add_block_to_chain(self, newBlock):
         newBlock.previousHash = self.get_latest_block().hash
-        newBlock.hash = newBlock.compute_hash()
+        newBlock.mine(self.difficulty)
         self.chain.append(newBlock)
     
     def validate_chain(self):
@@ -48,8 +71,11 @@ block2 = Block("transfer one hundred dollars", "")
 JustinChain.add_block_to_chain(block1)
 JustinChain.add_block_to_chain(block2)
 
-JustinChain.chain[1].data = "transfer two hundred dollars"
-JustinChain.chain[1].hash = JustinChain.chain[1].compute_hash()
+for i in range(len(JustinChain.chain)):
+    print(JustinChain.chain[i].hash)
+for i in range(len(JustinChain.chain)):
+    print(JustinChain.chain[i].previousHash)
+
 
 if(JustinChain.validate_chain()):
     print("validated chain")
